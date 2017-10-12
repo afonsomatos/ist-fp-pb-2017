d9af7ccc112457b69a1a48c9b832627570d001db
Afonso Matos <afonso@MacBook-Pro-de.local>
2017-10-12 09:26:25 +0100

    Update cap.3 ex_05

--- Capítulo 3/ex_05.py
+++ Capítulo 3/ex_05.py
@@ -2,24 +2,21 @@ def calcularDinheiro(valor):
     valor = int(valor * 10 * 10)
 
     itens = (
-        (50, 50 * 100, "Nota"),
-        (20, 20 * 100, "Nota"),
-        (10, 10 * 100, "Nota"),
-        (5, 5 * 100, "Nota"),
-        (2, 200, "Moeda"),
-        (1, 100, "Moeda"),
-        (50, 50, "Moeda"),
-        (20, 20, "Moeda"),
-        (10, 10, "Moeda"),
-        (5, 5, "Moeda"),
-        (2, 2, "Moeda"),
-        (1, 1, "Moeda")
+        ("Nota 50€", 50 * 100),
+        ("Nota 20€", 20 * 100),
+        ("Nota 10€", 10 * 100),
+        ("Nota 5€", 5 * 100),
+        ("Moeda 2€", 200),
+        ("Moeda 1€", 100),
+        ("Moeda 50 cêntimos", 50),
+        ("Moeda 20 cêntimos", 20),
+        ("Moeda 10 cêntimos", 10),
+        ("Moeda 5 cêntimos", 5),
+        ("Moeda 2 cêntimos", 2),
+        ("Moeda 1 cêntimo", 1)
     )
 
     for t in itens:
         quantia = valor // t[1]
         valor %= t[1]
-        print(t[2], "de", t[0], "-", quantia)
-
-
-calcularDinheiro(50.55)
+        print(t[0], "-", quantia)
